fee21adcaf4930a93b0e9f7d91a0c5de86897a56
Cenx <dfzy5566@qq.com>
2017-07-17 16:30:35 +0800

    fixed a bug: vinber_value t: false not workds

--- lib/vinber/vinber_value.rb
+++ lib/vinber/vinber_value.rb
@@ -5,13 +5,13 @@ module Vinber
       obj   = self
       klass = obj.class
       val   = obj.send(attribute.to_sym)
-      api   = options.fetch(:t) {false}
+      t     = options.fetch(:t) {true}
 
       if klass.vinber_defined?(attribute) &&
         (attribute_vinber = klass.defined_vinbers[attribute.to_s]).is_a?(Hash)
 
         attribute_vinber_key = attribute_vinber.key val
-        api ? attribute_vinber_key.to_s : Vinber::Translate.new(klass, attribute, attribute_vinber_key).to_s
+        t ? Vinber::Translate.new(klass, attribute, attribute_vinber_key).to_s : attribute_vinber_key.to_s
       else
         val
       end
